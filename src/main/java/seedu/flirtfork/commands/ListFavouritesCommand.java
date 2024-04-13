7dec071f770b5b9d13314e9c4f7a71b61b3c4b42
tiffanyliu0220 <tiffanyliu0220@gmail.com>
2024-04-13 23:52:35 +0800

    added javadoc to ListFavouritesCommand

--- src/main/java/seedu/flirtfork/commands/ListFavouritesCommand.java
+++ src/main/java/seedu/flirtfork/commands/ListFavouritesCommand.java
@@ -10,7 +10,22 @@ import seedu.flirtfork.Ui;
 import seedu.flirtfork.UserDetails;
 import seedu.flirtfork.exceptions.FlirtForkException;
 
+/**
+ * Command to list the user's favourites.
+ */
 public class ListFavouritesCommand extends Command {
+    /**
+     * Executes the list favourites command.
+     *
+     * @param favourites The list of favourites.
+     * @param foods      The list of food options.
+     * @param activities The list of activity options.
+     * @param ui         The user interface.
+     * @param storage    The storage component.
+     * @param userDetails The user details.
+     * @param gifts      The list of gift options.
+     * @throws FlirtForkException If an error occurs during execution.
+     */
     @Override
     public void execute(FavouritesList favourites, FoodList foods, ActivityList activities, Ui ui,
                         Storage storage, UserDetails userDetails, GiftList gifts) throws FlirtForkException {
