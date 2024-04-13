a05f735a558f74d61a1ba18778f06baecc393408
tiffanyliu0220 <tiffanyliu0220@gmail.com>
2024-04-14 00:01:08 +0800

    added javadoc to ExitCommand

--- src/main/java/seedu/flirtfork/commands/ExitCommand.java
+++ src/main/java/seedu/flirtfork/commands/ExitCommand.java
@@ -12,7 +12,22 @@ import seedu.flirtfork.exceptions.FlirtForkException;
 
 import java.io.IOException;
 
+/**
+ * Command to exit the application.
+ */
 public class ExitCommand extends Command {
+    /**
+     * Executes the list favourites command.
+     *
+     * @param favourites The list of favourites.
+     * @param foods      The list of food options.
+     * @param activities The list of activity options.
+     * @param ui         The user interface.
+     * @param storage    The storage component.
+     * @param userDetails The user details.
+     * @param gifts      The list of gift options.
+     * @throws FlirtForkException If an error occurs during execution.
+     */
     @Override
     public void execute(FavouritesList favourites, FoodList foods, ActivityList activities, Ui ui,
                         Storage storage, UserDetails userDetails, GiftList gifts) throws FlirtForkException {
